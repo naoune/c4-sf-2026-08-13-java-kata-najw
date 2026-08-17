243033f1f7daae2720a912d02f80fa2a471b4f32
AOUNE <najwa.aoune@capgemini.com>
2026-08-17 17:44:47 +0100

    chore: initialize spring boot project with web, kafka, validation, lombok, h2 and openapi

---
+++ src/main/java/com/najwa/kata/loyalty_kata/LoyaltyKataApplication.java
@@ -0,0 +1,13 @@
+package com.najwa.kata.loyalty_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/najwa/kata/loyalty_kata/LoyaltyKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.najwa.kata.loyalty_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
